cf7f13bfd666ccd53c5bca41abfdf8fd09d245a3
cucarachatraicionera <cucarachatraicionera@gmail.com>
2025-05-21 15:19:29 -0500

    Roadmap

--- app/components/Trade/index.tsx
+++ app/components/Trade/index.tsx
@@ -15,8 +15,8 @@ const Trade = () => {
                 {/* Column-2 */}
 
                 <div>
-                    <h3 className="text-3xl lg:text-5xl font-semibold text-offwhite mb-6 text-center sm:text-start">Trade Anywhere <br /> Any time</h3>
-                    <p className="lg:text-lg font-normal text-lightblue mb-16 text-center sm:text-start">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <h3 className="text-3xl lg:text-5xl font-semibold text-offwhite mb-6 text-center sm:text-start">R O A D M A P</h3>
+                    <p className="lg:text-lg font-normal text-lightblue mb-16 text-center sm:text-start">PinkyPromise is moving full speed ahead with its roadmap: after the successful launch of its $PKP token — now available for direct purchase on the official website — the team is currently focused on developing its core utilities, including the highly anticipated Aviator game and the international lottery platform. This phase reinforces the project’s vision of delivering a decentralized, fun, and rewarding experience for its community.</p>
                     <div className="flex justify-between">
                         <Image src={'/images/Trade/mac.svg'} alt="macOS-image" width={61} height={105} />
                         <div className="verticalLine"></div>
@@ -24,7 +24,7 @@ const Trade = () => {
                         <div className="verticalLine"></div>
                         <Image src={'/images/Trade/windows.svg'} alt="windows-image" width={80} height={105} />
                         <div className="verticalLine"></div>
-                        <Image src={'/images/Trade/android.svg'} alt="android-image" width={71} height={105} />
+                        <Image src={'/images/Trade/android.png'} alt="android-image" width={71} height={105} />
                     </div>
                 </div>
             </div>
